bf81404f8793d00cc5777b799f0a323e527dfb02
Horia <horia.marinescu12@gmail.com>
2022-08-08 19:39:28 +0300

    Header-Right

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import HeaderIcon from './HeaderIcon';
 
 import {
-  HomeIcon, UserGroupIcon,
+  HomeIcon, UserGroupIcon, ViewGridIcon, BellIcon, ChatIcon, ChevronDownIcon,
 } from '@heroicons/react/solid';
 
 import {
@@ -30,7 +30,10 @@ function Header() {
 
             <div className='flex nl-2 items-center rounded-full bg-gray-100 p-2'>
               <SearchIcon className='h-6 text-gray-600'/>
-              <input className='flex nl-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type="text" placeholder="Search" />
+              <input className='hidden md:inline-flex nl-2 items-center bg-transparent 
+              outline-none placeholder-gray-500 flex-shrink' 
+              type="text" 
+              placeholder="Search" />
             </div>
 
           </div>
@@ -41,13 +44,24 @@ function Header() {
 
             <div className='flex space-x-6 md:space-x-2'>
 
-              <HeaderIcon active Icon={HomeIcon} />
+              <HeaderIcon Icon={HomeIcon} active />
               <HeaderIcon Icon={FlagIcon} />
               <HeaderIcon Icon={PlayIcon} />
               <HeaderIcon Icon={ShoppingCartIcon} />
               <HeaderIcon Icon={UserGroupIcon} />
 
             </div>
+          </div>
+
+          {/* RIGHT */}
+          <div className='flex items-center sm:space-x-2 justify-end '>
+
+            <p className='whitespace-nowrap font-semibold pr-3'>USERNAME</p>
+            <ViewGridIcon className="icon" />
+            <ChatIcon className="icon" />
+            <BellIcon className="icon" />
+            <ChevronDownIcon className="icon" />
+
           </div>
 
     </div>
